c4518424eb9234b4b0fa56a303f23ffab07933f4
name <you@example.com>
2017-11-02 23:25:49 +0200

    init

---
+++ Readme.md
@@ -0,0 +1,15 @@
+#Files:
+- Readme.md
+- package.json  
+- main.js  
+
+- Procfile
+- app.json  
+
+#Deploying..
+To deploy the app to Heroku click the link and login
+<a href="https://heroku.com/deploy" > This </a>
+ or 
+[this] (https://heroku.com/deploy)
+
+
---
+++ main.js
@@ -0,0 +1,97 @@
+/*
+ * https://developers.facebook.com/docs/messenger-platform/getting-started/quick-start/
+ *
+ */
+
+'use strict';
+
+// Imports dependencies and set up http server
+const 
+  request = require('request'),
+  express = require('express'),
+  body_parser = require('body-parser'),
+  app = express().use(body_parser.json()); // creates express http server
+
+// for heroku
+app.set('port', (process.env.PORT || 5000));
+
+
+// test, unrelated to fb
+app.get('/', function(request, response) {
+  response.status(200).send("Hello world");
+});
+
+
+
+// Accepts POST requests at /webhook endpoint
+app.post('/webhook', function(req, res){  
+
+  // Parse the request body from the POST
+  let body = req.body;
+
+  // Check the webhook event is from a Page subscription
+  if (body.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+
+      // Get the webhook event. entry.messaging is an array, but 
+      // will only ever contain one event, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+      
+    });
+
+    // Return a '200 OK' response to all events
+    res.status(200).send('EVENT_RECEIVED');
+
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+
+});
+
+// Accepts GET requests at the /webhook endpoint
+app.get('/webhook', function(req, res){
+  // localhost:5000/webhook?hub.mode=subscribe&hub.verify_token=hi&hub.challenge=i
+  
+  /** UPDATE YOUR VERIFY TOKEN **/
+  const VERIFY_TOKEN = process.env.MY_VERIFY_TOKEN || "hi";
+  
+  // Parse params from the webhook verification request
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+   
+  
+  // Check if a token and mode were sent
+  if (mode && token) {
+  
+    // Check the mode and token sent are correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      
+      // Respond with 200 OK and challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      //res.sendStatus(403);  
+      res.status(403).send("mismatch!");    
+    }
+    
+  } else { // I added it
+	 res.status(403).send("wrong query");
+  }  
+});
+
+
+
+// heroku 
+app.listen(app.get('port'), function() {
+  console.log('Node app is running...');
+});
+
+//non heroku init
+// app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
